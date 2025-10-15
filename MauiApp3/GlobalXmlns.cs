47f89e5ed246cac9219db2f7ed5861a588996758
xHelpy <xiozeerpl@gmail.com>
2025-10-15 09:34:26 +0200

    2

---
+++ MauiApp3/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp3")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiApp3.Pages")]
---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SizerData.Text = "Rozmiar: " + (int)e.NewValue;
+            Sizer.FontSize = (int)e.NewValue;
+        }
+        int id = 0;
+        private void CounterBtn_Clicked(object sender, EventArgs e)
+        {
+            
+            string[] witajki = ["Dzień Dobry","Buenos Dias", "Good Morning"];
+            if(id > 2)
+            {
+                id = 0;
+                Sizer.Text = witajki[id];
+                id += 1;
+            } else if (id <= 2)
+            {
+                
+                Sizer.Text = witajki[id];
+                id += 1;
+            }
+        }
+    }
+}
